a401980be3e7c70bf20d4b96282a86614be18017
minamik <minamikik@users.noreply.github.com>
2026-07-08 00:25:52 +0900

    feat: support server_fastapi port override

--- config.py
+++ config.py
@@ -195,7 +195,7 @@ class Webui_config:
 class Server_config:
     def __init__(
         self,
-        port: int = 5000,
+        port: int = 5100,
         device: str = "cuda",
         limit: int = 100,
         language: str = "JP",
